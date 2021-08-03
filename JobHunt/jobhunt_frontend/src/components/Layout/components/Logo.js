1cbfaac5998b02fbb5a3c81738a5055ce7a909eb
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-08-03 16:58:23 +0430

    Make Logo Component

---
+++ JobHunt/jobhunt_frontend/src/components/Layout/components/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {logo} from '../../../asset/index'
+const Logo = () => {
+    return (
+        <>
+          <img style ={{height:'100%'}} src= {logo} /> 
+        </>
+    )
+}
+export {Logo}
