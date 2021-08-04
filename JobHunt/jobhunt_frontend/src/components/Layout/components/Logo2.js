bb55b87e78f0539624dbd49f8babe630290e9c3a
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-08-04 20:40:32 +0430

    Add Second Logo

---
+++ JobHunt/jobhunt_frontend/src/components/Layout/components/Logo2.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {logo2} from '../../../asset/index'
+const Logo2 = () => {
+    return (
+        <>
+          <img style ={{height:'100%'}} src= {logo2} /> 
+        </>
+    )
+}
+export {Logo2}
