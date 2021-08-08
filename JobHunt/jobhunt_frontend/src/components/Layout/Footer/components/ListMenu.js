07d02fd3c8b5a08a338a5cd0d4db53cdd1bba05f
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-08-09 00:05:30 +0430

    Create ListMenu Component

---
+++ JobHunt/jobhunt_frontend/src/components/Layout/Footer/components/ListMenu.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { Link, MenuItem, MenuList, makeStyles } from '@material-ui/core'
+const useClasses = makeStyles((item) => ({
+    root: {
+        '& .MuiLink-underlineHover:hover': {
+            textDecoration: 'none',
+        },
+        '& .MuiMenuItem-root':{
+            fontSize:'15px'
+        }
+    },
+    menuItem: {
+        boxSizing: 'box-content',
+        color: '#8a99b3',
+        position: 'relative',
+        transition: 'all 0.3s ease',
+        '&:before': {
+            backgroundColor: '#8a99b3',
+            content: '""',
+            height: '1px',
+            marginRight: '10px',
+            transition: 'all 0.3s ease',
+            width: '20px',
+        },
+        '&:hover': {
+            color: 'white',
+            '& .MuiSvgIcon-root': {
+                width: '20px'
+            },
+            '&:before': {
+                backgroundColor: 'white',
+                width: '35px',
+            }
+        }
+    },
+    link: {
+        color: 'inherit',
+        width: '100%',
+    }
+}))
+
+const ListMenu = (props) => {
+    const classes = useClasses()
+    console.log(props)
+    return (
+        <MenuList className={classes.root}>
+            {props.menuItems.map((item) => (
+                <MenuItem className={classes.menuItem} >
+                    <Link
+                        className={classes.link}
+                        href={item.href}>
+                        {item.name}
+                    </Link>
+                </MenuItem>
+            ))}
+        </MenuList>
+    )
+}
+export { ListMenu }
