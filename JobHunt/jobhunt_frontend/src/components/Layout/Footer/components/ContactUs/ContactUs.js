c66e1fad512afc84657fbace3aa4eccb75e808be
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-08-09 00:06:58 +0430

    Update ContactUs

--- JobHunt/jobhunt_frontend/src/components/Layout/Footer/components/ContactUs/ContactUs.js
+++ JobHunt/jobhunt_frontend/src/components/Layout/Footer/components/ContactUs/ContactUs.js
@@ -4,34 +4,34 @@ import { Logo } from '../../../components/Logo'
 import SocialMedias from './components/SocialMedias'
 import { makeStyles } from '@material-ui/core'
 import { classExpression } from '@babel/types'
-const useClasses =makeStyles ((theme)=>({
-  root:{
-      margin:theme.spacing(2),
-      padding:theme.spacing(2),
-    //   display:'flex',
-      flexDirection:'column',
-    //   alignItems:'space-between'
+const useClasses = makeStyles((theme) => ({
+  root: {
+    flexDirection: 'column',
+    margin: theme.spacing(2),
+    padding: theme.spacing(2),
+
   },
-  text:{
-      marginTop:theme.spacing(2) ,
-      marginBottom:theme.spacing(2),
-      color: '#8a99b3',
+  text: {
+    color: '#8a99b3',
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+
   }
 }))
 const ContactUs = (props) => {
-    const classes = useClasses()
-    return (
-        <Box className={classes.root} height="100%">
-            <Logo />
-            {/* Address */}
-            <Typography className={classes.text} variant="h6">Collin Street West, Victor 8007, Australia.</Typography>
-            {/* Phone Number */}
-            <Typography className={classes.text} variant="h6">+1 246-345-0695</Typography>
-            {/* Email */}
-            <Typography className={classes.text} variant="h6">info@jobhunt.com</Typography>
-            <SocialMedias/>
+  const classes = useClasses()
+  return (
+    <Box className={classes.root} height="100%">
+      <Logo />
+      {/* Address */}
+      <Typography className={classes.text} variant="h6">Collin Street West, Victor 8007, Australia.</Typography>
+      {/* Phone Number */}
+      <Typography className={classes.text} variant="h6">+1 246-345-0695</Typography>
+      {/* Email */}
+      <Typography className={classes.text} variant="h6">info@jobhunt.com</Typography>
+      <SocialMedias />
 
-        </Box>
-    )
+    </Box>
+  )
 }
 export default ContactUs
