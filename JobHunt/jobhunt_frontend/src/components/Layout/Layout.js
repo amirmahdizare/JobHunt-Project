b056b8a3c27d2706bba27db6710e871819367ebc
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-08-09 00:15:48 +0430

    Add Footer To Layout

--- JobHunt/jobhunt_frontend/src/components/Layout/Layout.js
+++ JobHunt/jobhunt_frontend/src/components/Layout/Layout.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { DesktopHeader } from './Header/DesktopView/DesktopHeader'
 import { MobileHeader } from './Header/MobileView/MobileHeader'
-const Layout = (props) => {
+import Footer from './Footer/Footer'
+const Layout = () => {
     return (
         <>
             <DesktopHeader />
             <MobileHeader />
+            <Footer/>
         </>
     )
 }
