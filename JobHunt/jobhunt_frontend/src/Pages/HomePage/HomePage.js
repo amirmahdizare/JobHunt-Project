ec5f6e2ce16c581747b7154016efab746f291838
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-08-10 23:08:03 +0430

    Add SearchSection To HomePage

---
+++ JobHunt/jobhunt_frontend/src/Pages/HomePage/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import SearchSection from './components/SearchSection/SearchSection'
+const HomePage = () => {
+    return (
+        <div>
+            <SearchSection/>
+        </div>
+    )
+}
+export {HomePage}
