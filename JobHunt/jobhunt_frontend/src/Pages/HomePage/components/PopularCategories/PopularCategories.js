eee60c21261940cefdba029afa803b0b5fc9fb22
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-08-12 15:18:23 +0430

    Create PopularCategories

---
+++ JobHunt/jobhunt_frontend/src/Pages/HomePage/components/PopularCategories/PopularCategories.js
@@ -0,0 +1,47 @@
+import { Box, Button, Container, Typography } from '@material-ui/core'
+import React from 'react'
+import CategoriesContainer from './components/CategoriesContainer'
+import { makeStyles } from '@material-ui/core'
+import JumpButton from '../JumpButton/JumpButton'
+const useClasses = makeStyles(theme => ({
+    root: {
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        textAlign: 'center',
+        color:'black',
+        zIndex:'100',
+        position:'relative'
+    }
+    ,
+    button: {
+        borderRadius: theme.spacing(1),
+        border: '2px solid',
+        borderColor: theme.palette.secondary.main,
+        color: theme.palette.text.primary,
+        marginBottom: theme.spacing(5),
+        padding: theme.spacing(1.5),
+        textTransform: 'none',
+        transition: 'all .4s ease',
+
+        '&:hover': {
+            backgroundColor: theme.palette.secondary.main,
+            border: '2px solid',
+            borderColor: theme.palette.secondary.main,
+            color: theme.palette.secondary.contrastText
+        }
+    }
+}))
+const PopularCategories = () => {
+    const classes = useClasses()
+    return (
+        <Container maxWidth="lg" className={classes.root}>
+            <JumpButton/>
+            <Box id="categories" mb={3} mt={10}><Typography variant="h3">Popular Categories</Typography></Box>
+            <Box  ><Typography variant="h5">37 jobs live - 0 added today.</Typography></Box>
+            <CategoriesContainer />
+            <Button  className={classes.button} color="secondary" variant="outlined"><Typography variant="h6" style={{fontSize:'16px'}} >Browse All Categories</Typography></Button>
+        </Container>
+    )
+}
+export default PopularCategories
