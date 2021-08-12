dec44d4528366923cb52d5d799643946a412dcc2
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-08-12 18:02:48 +0430

    Apply Responsivefontsize to project with theme

--- JobHunt/jobhunt_frontend/src/components/Layout/Layout.js
+++ JobHunt/jobhunt_frontend/src/components/Layout/Layout.js
@@ -2,13 +2,26 @@ import React from 'react'
 import { DesktopHeader } from './Header/DesktopView/DesktopHeader'
 import { MobileHeader } from './Header/MobileView/MobileHeader'
 import Footer from './Footer/Footer'
+import { HomePage } from '../../Pages/HomePage/HomePage.js'
+import { Box } from '@material-ui/core'
+import { createTheme, responsiveFontSizes, ThemeProvider } from '@material-ui/core/styles';
+
+let theme = createTheme({
+    typography: {
+        fontFamily: 'Open Sans',
+    }
+});
+theme = responsiveFontSizes(theme);
 const Layout = () => {
     return (
-        <>
+        <Box style={{ boxSizing: 'border-box' }}>
+            <ThemeProvider theme={theme}>
             <DesktopHeader />
             <MobileHeader />
-            <Footer/>
-        </>
+            <HomePage />
+            <Footer />
+            </ThemeProvider>
+        </Box>
     )
 }
 export { Layout }
