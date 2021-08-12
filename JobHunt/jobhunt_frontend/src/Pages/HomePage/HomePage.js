9d08e4947ceae0ff3ebc6621a9fa1aeb136c2c87
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-08-12 15:19:34 +0430

    Add PopularCategories to HomePage

--- JobHunt/jobhunt_frontend/src/Pages/HomePage/HomePage.js
+++ JobHunt/jobhunt_frontend/src/Pages/HomePage/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import SearchSection from './components/SearchSection/SearchSection'
+import PopularCategories from './components/PopularCategories/PopularCategories'
 const HomePage = () => {
     return (
         <div>
             <SearchSection/>
+            <PopularCategories/>
         </div>
     )
 }
