a04142c83f8ce7de19c73db4507c3264ecbee89d
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-08-13 14:10:44 +0430

    Add FeaturedJobs to HomePage

--- JobHunt/jobhunt_frontend/src/Pages/HomePage/HomePage.js
+++ JobHunt/jobhunt_frontend/src/Pages/HomePage/HomePage.js
@@ -2,13 +2,14 @@ import React from 'react'
 import SearchSection from './components/SearchSection/SearchSection'
 import PopularCategories from './components/PopularCategories/PopularCategories'
 import JobHuntResume from './components/JobHuntResume/JobHuntResume'
-
+import FeaturedJobs from './components/FeaturedJobs/FeaturedJobs'
 const HomePage = () => {
     return (
         <div>
                 <SearchSection />
                 <PopularCategories />
                 <JobHuntResume />
+                <FeaturedJobs/>
         </div>
     )
 }
