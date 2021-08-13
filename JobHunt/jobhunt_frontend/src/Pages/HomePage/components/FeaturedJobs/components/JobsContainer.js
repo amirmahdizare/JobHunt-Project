98b87819008bb2187bad1b50fdf2c1c85c5b22db
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-08-13 14:09:57 +0430

    Create JobsContainer

---
+++ JobHunt/jobhunt_frontend/src/Pages/HomePage/components/FeaturedJobs/components/JobsContainer.js
@@ -0,0 +1,71 @@
+import { Box, makeStyles } from '@material-ui/core'
+import React from 'react'
+import JobCard from './JobCard'
+const useClasses = makeStyles((theme) => ({
+    root: {
+        margin: theme.spacing(5),
+        width: '100%'
+        ,
+        '& .MuiCard-root:not(:last-child)': {
+            borderBottom: '1px solid #e2e2e2'
+        }
+    }
+
+}))
+const JobsContainer = () => {
+    const classes = useClasses()
+    return (
+        <Box className={classes.root}>
+            {/* Worktime Must be one of these items  "FULL TIME" "PART TIME" "FREELANCE" "TEMPORARY" */}
+            <JobCard
+                jobTitle="Web Designer / Developer"
+                companyName="Massimo Artemisis"
+                companyLocation="Sacramento, California"
+                workTime="FULL TIME"
+                companyLogo="https://creativelayers.net/themes/jobhunt-html/images/resource/l1.png"
+            />
+
+            <JobCard
+                jobTitle="Marketing Director"
+                companyName="Tix Dog"
+                companyLocation="Rennes, France"
+                workTime="PART TIME"
+                companyLogo="https://creativelayers.net/themes/jobhunt-html/images/resource/l2.png"
+            />
+
+
+            <JobCard
+                jobTitle="C Developer (Senior) C .Net"
+                companyName="StarHealth"
+                companyLocation="London, United Kingdom"
+                workTime="FULL TIME"
+                companyLogo="https://creativelayers.net/themes/jobhunt-html/images/resource/l3.png"
+            />
+
+
+            <JobCard
+                jobTitle="Application Developer For Android"
+                companyName="Altes Bank"
+                companyLocation="Istanbul, Turkey"
+                workTime="FREELANCE"
+                companyLogo="https://creativelayers.net/themes/jobhunt-html/images/resource/l4.png"
+            />
+            <JobCard
+                jobTitle="Regional Sales Manager South east Asia"
+                companyName="Vincent"
+                companyLocation="Ajax, Ontario"
+                workTime="TEMPORARY"
+                companyLogo="https://creativelayers.net/themes/jobhunt-html/images/resource/l1.png"
+            />
+
+            <JobCard
+                jobTitle="Social Media and Public Relation Executive"
+                companyName="MediaLab"
+                companyLocation="Ankara / Turkey"
+                workTime="FULL TIME"
+                companyLogo="https://creativelayers.net/themes/jobhunt-html/images/resource/l6.png"
+            />
+        </Box>
+    )
+}
+export default JobsContainer
