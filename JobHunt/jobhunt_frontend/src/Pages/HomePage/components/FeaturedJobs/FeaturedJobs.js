d13e02c9d1c69bcb0c6464fffdc95c0cf7f6905a
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-08-13 14:10:24 +0430

    Create FeaturedJobs Section

---
+++ JobHunt/jobhunt_frontend/src/Pages/HomePage/components/FeaturedJobs/FeaturedJobs.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Box,Button, Container, Typography, makeStyles } from '@material-ui/core'
+import JobsContainer from './components/JobsContainer'
+const useClasses = makeStyles(theme => ({
+    root: {
+        position: 'relative',
+        textAlign: 'center',
+    }
+    ,
+    button: {
+        borderRadius: theme.spacing(1),
+        border: '2px solid',
+        borderColor: theme.palette.secondary.main,
+        color: theme.palette.text.primary,
+        marginBottom: theme.spacing(5),
+        padding: theme.spacing(1.5),
+        textTransform: 'none',
+        transition: 'all .4s ease',
+        '&:hover': {
+            backgroundColor: theme.palette.secondary.main,
+            border: '2px solid',
+            borderColor: theme.palette.secondary.main,
+            color: theme.palette.secondary.contrastText
+        }
+    }
+    ,
+    container: {
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        position: 'relative',
+        width: '100%',
+    }
+}))
+const FeaturedJobs = () => {
+    const classes = useClasses()
+    return (
+        <Box width="100%" className={classes.root}>
+            <Container maxWidth="lg" className={classes.container}>
+                <Box marginTop={6}>
+                    <Typography variant="h3">Featured Jobs</Typography>
+                    <Typography variant="h6">Leading Employers already using job and talent.</Typography>
+                </Box>
+                <JobsContainer />
+                <Button  className={classes.button} color="secondary" variant="outlined"><Typography variant="h6" style={{fontSize:'16px'}} >Load more listings</Typography></Button>
+            </Container>
+        </Box>
+    )
+}
+export default FeaturedJobs
