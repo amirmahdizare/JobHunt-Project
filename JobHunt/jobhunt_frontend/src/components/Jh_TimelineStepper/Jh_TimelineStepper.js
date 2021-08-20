0b946d20f6243770d1ff05cf41e79a904a8973f3
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-08-20 17:12:50 +0430

    Create Timeline-stepper Component

---
+++ JobHunt/jobhunt_frontend/src/components/Jh_TimelineStepper/Jh_TimelineStepper.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Box , makeStyles  } from '@material-ui/core'
+import { Jh_TimelineStepperItem } from './Jh_TimelineStepperItem'
+const useClasses=makeStyles(theme=>({
+    root:{
+        '& .Jh_TimelineStepperItem:not(:last-child):before':{
+            position:'absolute',
+            left:'27px',
+            backgroundColor:theme.palette.primary.main,
+            content:'""',
+            width:'2px',
+            height:'100%',
+            zIndex:-1,
+            overflow:'hidden'
+        }
+    }
+}))
+export const Jh_TimeLineStepper = (props) => {
+    const classes=useClasses()
+    // console.log(props)
+    return (
+        <Box className={classes.root} >
+            {props.items.map((item)=>(
+                <Jh_TimelineStepperItem 
+                title={item.title}
+                secondTitle={item.secondTitle}
+                duration={item.duration}
+                description={item.description}/>
+            ))}
+            
+        </Box>
+    )
+}
---
+++ JobHunt/jobhunt_frontend/src/components/Jh_TimelineStepper/Jh_TimelineStepperItem.js
@@ -0,0 +1,42 @@
+import { Box, Grid } from '@material-ui/core'
+import React from 'react'
+import { Paper, Typography, Tooltip, IconButton, makeStyles } from '@material-ui/core'
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import { LineAwesome, SvgGraduationCapSolid, SvgPencilAltSolid, SvgTrashAlt } from 'react-line-awesome-svg'
+import { Jh_Tooltip } from '../Jh_Tooltip';
+const useClasses = makeStyles(theme => ({
+    secondTitle:{
+        [theme.breakpoints.up('sm')]:{
+            display:'inline'
+        },
+    }
+}))
+export const Jh_TimelineStepperItem = (props) => {
+    console.log(props)
+    const classes = useClasses()
+    return (
+        <Box p={2} className="Jh_TimelineStepperItem" display="flex" flex="1" position="relative" >
+           
+            {/* Logo */}
+            <RadioButtonUncheckedIcon style={{ backgroundColor:'white' }} color="primary" />
+                   <Box width="100%" paddingX={2} position="relative">
+            <Grid container>
+               <Grid item xs={11}>
+                {/* Info */}
+                <Typography color="primary" display="inline" variant="h6" gutterBottom>{props.title}&nbsp;&nbsp;&nbsp;</Typography>
+                <Typography color="textSecondary" className={classes.secondTitle}  variant="body1" gutterBottom>{props.secondTitle}</Typography>
+                <Typography color="textSecondary" variant="body1" gutterBottom>{props.duration}</Typography>
+                <Typography variant="subtitle2" color="textSecondary">{props.description}</Typography>
+                </Grid>
+                <Grid item xs={1}>
+                {/* Tooltip */}
+                <Box display="flex" justifyContent="center" >
+                    <Jh_Tooltip laIcon={SvgPencilAltSolid} title="Edit" callback={undefined} />
+                    <Jh_Tooltip laIcon={SvgTrashAlt} title="Delete" callback={undefined} />
+                </Box>
+                </Grid >
+            </Grid>
+            </Box>
+        </Box>
+    )
+}
