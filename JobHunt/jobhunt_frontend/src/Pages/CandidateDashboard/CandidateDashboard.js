c3c808a1f0f7daf7dcf119a8ee6f317787f81c82
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-08-20 17:16:47 +0430

    Update Candiadate Dashboard

---
+++ JobHunt/jobhunt_frontend/src/Pages/CandidateDashboard/CandidateDashboard.js
@@ -0,0 +1,13 @@
+import { Box, Container, Grid ,makeStyles} from '@material-ui/core'
+import React from 'react'
+import { Header } from './components/Header'
+import { Content } from './components/Content/Content'
+export const CandidateDashboard = () => {
+    return (
+        <Box>
+             <Header/>
+             <Content/>
+   
+        </Box>
+    )
+}
