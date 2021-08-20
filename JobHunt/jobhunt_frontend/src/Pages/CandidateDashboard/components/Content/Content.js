59ebe3f91dfb5fc26d04628c25391767f414cf00
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-08-20 17:16:29 +0430

    Create Candiadate Dashboard > Content

---
+++ JobHunt/jobhunt_frontend/src/Pages/CandidateDashboard/components/Content/Content.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Box, Container, Divider, Grid ,makeStyles, Typography} from '@material-ui/core'
+import  {Menu}  from './components/Menu'
+import { SkillsPercentage } from './components/SkillsPercentage'
+import { Education } from './components/Education/Education'
+import { WorkExperience } from './components/WorkExperience/WorkExperience'
+import { Portfolio } from './components/Portfolio/Portfolio'
+import { ProfessionalSkills } from './components/ProfessionalSkills/ProfessionalSkills'
+import { CertificatesSection } from './components/CertificatesSection/CertificatesSection'
+export const Content = () => {
+    return (
+        <Container>
+                <Grid container direction="row">
+                    <Grid item xs={12} lg={3}>
+                        <Menu />
+                        <Divider light/>
+                        <SkillsPercentage/>
+                    </Grid>
+                    <Divider orientation="vertical" flexItem/>
+                    <Grid item xs={12} lg={8}>
+                        <Box p={1}><Typography variant="h4" color="primary">Candidate Dashboard</Typography></Box>
+                        <Divider light/>
+                        <Education/>
+                        <WorkExperience/>
+                        <Portfolio/>
+                        <ProfessionalSkills/>
+                        <CertificatesSection/>
+
+                    </Grid>
+                </Grid>
+
+
+            </Container>
+    )
+}
