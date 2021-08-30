4685e4539fc75b596c00c126e52f3e32b8184040
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-08-30 18:19:01 +0430

    Create EmployerListPage > SelectLocation

--- JobHunt/jobhunt_frontend/src/Pages/EmployerList/components/SelectLocation.js
+++ JobHunt/jobhunt_frontend/src/Pages/EmployerList/components/SelectLocation.js
@@ -1,15 +1,8 @@
-import { IconButton, InputBase,makeStyles } from '@material-ui/core'
+import { IconButton, InputBase } from '@material-ui/core'
 import React from 'react'
 import { Jh_Card } from '../../../components/Jh_Card'
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-const useStyles=makeStyles(theme=>({
-    // iconButton:{
-    //     paddingRight:'6px'
-    // }
-}))
-
 export const SelectLocation = () => {
-    const classes=useStyles()
     return (
         <Jh_Card>
             <InputBase
@@ -17,7 +10,7 @@ export const SelectLocation = () => {
                 placeholder="Select Location"
                 inputProps={{ 'aria-label': 'Select Location' }}
             />
-            <IconButton disableFocusRipple classes={{root:classes.iconButton}}  type="submit"  aria-label="search">
+            <IconButton disableFocusRipple   type="submit"  aria-label="search">
                 <LocationOnOutlinedIcon color="secondary" />
             </IconButton>
         </Jh_Card>
