1913430e3518824139e5f13d866ebae0975e6388
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-08-30 18:22:51 +0430

    Create EmployerListPage > EmployerCard

---
+++ JobHunt/jobhunt_frontend/src/Pages/EmployerList/components/Employers/components/EmployerCard.js
@@ -0,0 +1,64 @@
+import { Card, CardActionArea, CardContent, CardMedia, Grid, makeStyles, Typography } from '@material-ui/core'
+import React from 'react'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+const useClasses = makeStyles(theme => ({
+
+    cardRoot: {
+        boxShadow:'unset',
+        border:'2px solid transparent',
+        '&:hover':{
+            backgroundColor:'unset',
+            border:'2px solid #e2e2e2'
+        }
+    },
+    actionArea:{
+        padding: theme.spacing(2),
+    },
+    openPosition: {
+        [theme.breakpoints.up('sm')]: {
+            position: 'absolute',
+            right: '10px'
+        }
+    },
+    cardMedia:{
+        width:'80px',
+        height:'80px',
+        borderRadius:theme.spacing(0.5),
+        border:'1px solid #e2e2e2',
+        float:'right'
+    },
+    alignItemsinXs:{
+        [theme.breakpoints.only('xs')]:
+        {display:'flex',alignItems:'center',flexDirection:'column',textAlign:'center'}
+    }
+}))
+export const EmployerCard = (props) => {
+    const classes = useClasses()
+    return (
+        <Card className={classes.cardRoot}>
+            <CardActionArea className={classes.actionArea} >
+                <Grid container spacing={3}>
+                    <Grid item xs={12} sm={2} className={classes.alignItemsinXs} justifyContent="flex-end">
+
+                        <CardMedia
+                        className={classes.cardMedia}
+                            image={props.logo}
+                            // height="80px"
+                            component="img" />
+                    </Grid>
+                    <Grid item xs={12} sm={10}  className={classes.alignItemsinXs}>
+                        {/* <CardContent> */}
+                        <Typography className={classes.openPosition} color="primary" gutterBottom>{props.numberOfOpenPositions}&nbsp;Open Position</Typography>
+                        <Typography variant="h6" gutterBottom>{props.name}</Typography>
+                        <Typography color="secondary"gutterBottom>{props.category}</Typography>
+                        <Typography gutterBottom variant="body2"><LocationOnOutlinedIcon color="action" style={{ verticalAlign: 'middle' }} />&nbsp;{props.location}</Typography>
+                        <Typography variant="body2" color="textSecondary" gutterBottom>{props.description}</Typography>
+
+                        {/* </CardContent> */}
+                    </Grid>
+
+                </Grid>
+            </CardActionArea>
+        </Card>
+    )
+}
