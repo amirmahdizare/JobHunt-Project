cad854b75528f446bd28fa270e918ea58648a462
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-01 12:19:23 +0430

    Create blogFrame > PostCard

---
+++ JobHunt/jobhunt_frontend/src/components/Jh_BlogFrame/components/RecentPosts.js/components/PostCard.js
@@ -0,0 +1,38 @@
+import { Box, Card, CardActionArea, CardContent, CardMedia, Link, makeStyles, MenuItem, Typography } from '@material-ui/core'
+import React from 'react'
+const useStyles = makeStyles(theme => ({
+    media: {
+        borderRadius: theme.spacing(1),
+        marginRight: theme.spacing(1),
+        height: '64px',
+        width: '74px',
+
+    },
+    cardRoot: {
+        boxShadow: 'unset',
+        background: 'transparent',
+        overflow: 'unset',
+    },
+    menuItem:{
+        display:'flex',
+        '&:hover':{
+            color:theme.palette.secondary.main
+        }
+    }
+}))
+export const PostCard = (props) => {
+    const { title, date, image } = props
+    const classes = useStyles()
+    return (
+        <MenuItem className={classes.menuItem} component={Link} color="textPrimary" href="#" >
+            <Card classes={{ root: classes.cardRoot }}>
+                <CardMedia classes={{ media: classes.media }} image={image} component="img" />
+            </Card>
+            <Box>
+                <Typography color="inherit" style={{ whiteSpace: 'pre-wrap' }}  variant="body2">{title}</Typography>
+                <Typography color="textSecondary" variant="subtitle2">{date}</Typography>
+                </Box>
+
+        </MenuItem>
+    )
+}
