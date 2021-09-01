82fcecbed893ae2a5f3e2151e9f541574114b109
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-01 12:18:05 +0430

    Create blogFrame > SearchKeyword

---
+++ JobHunt/jobhunt_frontend/src/components/Jh_BlogFrame/components/SearchKeywordBlog.js
@@ -0,0 +1,19 @@
+import { IconButton, InputBase
+} from '@material-ui/core'
+import React from 'react'
+import { Jh_Card } from '../../Jh_Card'
+import SearchIcon from '@material-ui/icons/Search';
+export const SearchKeywordBlog = () => {
+   return (
+       <Jh_Card>
+           <InputBase
+               style={{  flex: 1 }}
+               placeholder="Search Keyword"
+               inputProps={{ 'aria-label': 'Search Keyword' }}
+           />
+           <IconButton  type="submit"  aria-label="search">
+               <SearchIcon color="secondary" />
+           </IconButton>
+       </Jh_Card>
+   )
+}
