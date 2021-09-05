3b4b368e7c2dfb506fc3dcb982a8a34c2ebbe503
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-06 00:02:37 +0430

    Create Share Component

---
+++ JobHunt/jobhunt_frontend/src/components/Jh_Share.js
@@ -0,0 +1,24 @@
+import { Box, Typography } from '@material-ui/core'
+import React from 'react'
+import { LineAwesome, SvgFacebook, SvgGoogle, SvgTwitter } from 'react-line-awesome-svg'
+import { Jh_Button } from '../components/Jh_Button'
+
+export const Jh_Share = (props) => {
+    return (
+        <Box display="flex" alignItems="center">
+        <Box display={{xs:'none',sm:'block'}}><Typography >Share&nbsp;&nbsp;&nbsp;</Typography></Box>
+        <Jh_Button
+            href={props.googleLink}
+            color="secondary"
+            icon={<LineAwesome icon={SvgGoogle} />}></Jh_Button>
+        <Jh_Button
+            href={props.TwitterLink}
+            color="primary"
+            icon={<LineAwesome icon={SvgTwitter} />}></Jh_Button>
+        <Jh_Button
+            href={props.facebookLink}
+            color="warning"
+            icon={<LineAwesome icon={SvgFacebook} />}></Jh_Button>
+    </Box>
+    )
+}
