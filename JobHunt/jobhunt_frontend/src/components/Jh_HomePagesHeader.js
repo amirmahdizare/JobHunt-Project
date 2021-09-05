a7878fbe0b53bbdba119439123338dc06038bde1
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-06 01:28:20 +0430

    Create HomePageHeader Component

---
+++ JobHunt/jobhunt_frontend/src/components/Jh_HomePagesHeader.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react'
+import { Box, Breadcrumbs, Container, Link, makeStyles, Typography } from '@material-ui/core'
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+const useStyles = makeStyles(theme => ({
+    container: {
+        alignItems:'center',
+        display:'flex',
+        justifyContent:'space-between',
+        paddingTop:theme.spacing(15),
+        paddingBottom:theme.spacing(8),
+        [theme.breakpoints.down('sm')]:{
+            alignItems:'center',
+            justifyContent:'space-around',
+            flexDirection:'column',
+            textAlign:'center',
+            padding:theme.spacing(5)
+        }
+    },
+    root:{
+        backgroundColor:'#f4f5fa',
+    }
+}))
+export const Jh_HomePagesHeader = (props) => {
+    const {page ,description} = props
+    useEffect(()=>{
+            window.pageYOffset=1
+            window.scrollTo(0,1)
+    }) 
+    const classes = useStyles()
+    return (
+        <Box  className={classes.root} >
+            <Container maxWidth="lg" className={classes.container}>
+            <Box>
+                <Typography variant="h4">{page}</Typography>
+                <Typography color="textSecondary">{description}</Typography>
+            </Box>
+            <Breadcrumbs separator={<NavigateNextIcon fontSize="small" />} aria-label="breadcrumb">
+                <Link underline="none"  color="inherit" href="/" >
+                    Home
+                </Link>
+                {/* <Link underline="none" color="inherit" href="/" > */}
+                    <Typography> Pages</Typography>
+                {/* </Link> */}
+                <Typography color="textPrimary">{page}</Typography>
+            </Breadcrumbs>
+        </Container>
+        </Box>
+    )
+}
