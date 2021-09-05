d610480b176c40d10fddccad1fe4be52842f1807
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-06 00:40:35 +0430

    Add Quick Tips to About Us

--- JobHunt/jobhunt_frontend/src/Pages/AboutUs/AboutUs.js
+++ JobHunt/jobhunt_frontend/src/Pages/AboutUs/AboutUs.js
@@ -4,6 +4,7 @@ import { AboutUsDescription } from './components/AboutUsDescription'
 import { AboutUsHeader } from './components/AboutUsHeader'
 import { Jh_HappyCandidates } from '../../components/Jh_HappyCandidates/Jh_HappyCandidates'
 import { Jh_Statistics } from '../../components/Jh_Statistics'
+import { Jh_QuickCareerTips} from '../../components/Jh_QuickCareerTips/Jh_QuickCareerTips'
 export const AboutUs = () => {
   
     return (
@@ -12,6 +13,7 @@ export const AboutUs = () => {
             <AboutUsDescription/>
             <Jh_HappyCandidates/>
             <Jh_Statistics/>
+            <Jh_QuickCareerTips/>
             
         </Box>
     )
