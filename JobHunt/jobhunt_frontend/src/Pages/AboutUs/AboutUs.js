18174a1eea992bc972ce157aec963f73d168dc76
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-06 00:09:25 +0430

    Add Happy Candidate To About Us

--- JobHunt/jobhunt_frontend/src/Pages/AboutUs/AboutUs.js
+++ JobHunt/jobhunt_frontend/src/Pages/AboutUs/AboutUs.js
@@ -2,13 +2,14 @@ import { Box } from '@material-ui/core'
 import React from 'react'
 import { AboutUsDescription } from './components/AboutUsDescription'
 import { AboutUsHeader } from './components/AboutUsHeader'
-
+import { Jh_HappyCandidates } from '../../components/Jh_HappyCandidates/Jh_HappyCandidates'
 export const AboutUs = () => {
   
     return (
         <Box>
             <AboutUsHeader/>
             <AboutUsDescription/>
+            <Jh_HappyCandidates/>
             
         </Box>
     )
