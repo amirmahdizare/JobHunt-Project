0ce9381cc529cee63334b6a23d131d7f5894838b
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-06 01:28:48 +0430

    Add HomePagesHeader to About Me

--- JobHunt/jobhunt_frontend/src/Pages/AboutUs/AboutUs.js
+++ JobHunt/jobhunt_frontend/src/Pages/AboutUs/AboutUs.js
@@ -1,16 +1,16 @@
 import { Box } from '@material-ui/core'
 import React from 'react'
 import { AboutUsDescription } from './components/AboutUsDescription'
-import { AboutUsHeader } from './components/AboutUsHeader'
 import { Jh_HappyCandidates } from '../../components/Jh_HappyCandidates/Jh_HappyCandidates'
 import { Jh_Statistics } from '../../components/Jh_Statistics'
 import { Jh_QuickCareerTips} from '../../components/Jh_QuickCareerTips/Jh_QuickCareerTips'
 import { Jh_Supporters } from '../../components/Jh_Supporters/Jh_Supporters'
+import { Jh_HomePagesHeader } from '../../components/Jh_HomePagesHeader'
 export const AboutUs = () => {
   
     return (
         <Box>
-            <AboutUsHeader/>
+            <Jh_HomePagesHeader page="About Us" description="Keep up to date with the latest news"/>
             <AboutUsDescription/>
             <Jh_HappyCandidates/>
             <Jh_Statistics/>
