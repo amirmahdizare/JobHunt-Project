1736a27a4e4d9ecd15ebcb7b104cffc381cecc5e
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-06 02:23:56 +0430

    Create Contact Us Page

---
+++ JobHunt/jobhunt_frontend/src/Pages/ContactUs/ContactUs.js
@@ -0,0 +1,19 @@
+import { Box, Container, Grid } from '@material-ui/core'
+import React from 'react'
+import { Jh_HomePagesHeader } from '../../components/Jh_HomePagesHeader'
+import { JobHuntOffice } from './components/JobHuntOffice'
+import { KeepInTouch } from './components/KeepInTouch'
+
+export const ContactUs = () => {
+    return (
+        <Box>
+            <Jh_HomePagesHeader page="Contact" description="Keep up to date with the latest news"/>
+            <Container maxWidth="lg" >
+            <Grid container spacing={4}>
+                <KeepInTouch/>
+                <JobHuntOffice/>
+            </Grid>
+            </Container>
+        </Box>
+    )
+}
