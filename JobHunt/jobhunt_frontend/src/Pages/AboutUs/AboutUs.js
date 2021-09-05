f567c472eba7e4f658adbb1eddc875a35d56c261
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-05 18:48:43 +0430

    Add About UsHeader to About Us

---
+++ JobHunt/jobhunt_frontend/src/Pages/AboutUs/AboutUs.js
@@ -0,0 +1,13 @@
+import { Box } from '@material-ui/core'
+import React from 'react'
+import { AboutUsHeader } from './components/AboutUsHeader'
+
+export const AboutUs = () => {
+  
+    return (
+        <Box>
+            <AboutUsHeader/>
+            
+        </Box>
+    )
+}
