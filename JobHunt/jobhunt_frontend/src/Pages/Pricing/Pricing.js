0b798385fc36955ee7973545cf6009b7b09b208e
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-06 14:58:38 +0430

    Add Header To Pricing Page

---
+++ JobHunt/jobhunt_frontend/src/Pages/Pricing/Pricing.js
@@ -0,0 +1,15 @@
+import { Box, Container } from '@material-ui/core'
+import React from 'react'
+import { Jh_HomePagesHeader } from '../../components/Jh_HomePagesHeader'
+
+export const Pricing = () => {
+    return (
+        <Box>
+            <Jh_HomePagesHeader page="pricing" description="Keep up to date with the latest news"/>
+            <Container maxWidth="lg">
+                
+            </Container>
+            
+        </Box>
+    )
+}
