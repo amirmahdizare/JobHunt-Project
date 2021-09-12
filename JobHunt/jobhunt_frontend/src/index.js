1e17ccc099a89ea2e990bb704b4f1d30548c2e8a
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-12 17:49:56 +0430

    Update Main index.js

--- JobHunt/jobhunt_frontend/src/index.js
+++ JobHunt/jobhunt_frontend/src/index.js
@@ -2,26 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-// import {Jh_MainLayout} from './components/Jh_layout/Jh_MainLayout/Jh_MainLayout'
-// import { HomePage } from './Pages/HomePage/HomePage';
-// import {CandidateDashboard} from './Pages/CandidateDashboard/CandidateDashboard'
-// import {Jh_CandidateLayout} from './components/Jh_layout/Jh_CandidateLayout/Jh_CandidateLayout'
 import {Jh_EmployerLayout} from './components/Jh_layout/Jh_EmployerLayout/Jh_EmployerLayout'
-// import { EmployerManageJobs } from './Pages/EmployerManageJobs/EmployerManageJobs';
-// import { EmployerSingle } from './Pages/EmployerSingle/EmployerSingle';
-import {EmployerList} from './Pages/EmployerList/EmployerList'
-// import {BlogList} from './Pages/BlogList/BlogList'
-import {BlogSingle} from './Pages/BlogSingle/BlogSingle'
-import { BlogList } from './Pages/BlogList/BlogList';
-import {AboutUs} from './Pages/AboutUs/AboutUs'
-import { HomePage } from './Pages/HomePage/HomePage';
-import { ContactUs } from './Pages/ContactUs/ContactUs';
-import {FAQ} from './Pages/FAQ/FAQ';
-import { Pricing } from './Pages/Pricing/Pricing';
-import { Terms } from './Pages/Terms/Terms';
-import { HowItWorks } from './Pages/HowItWorks/HowItWorks';
-  // <Jh_MainLayout children={<HomePage/>}/> 
-  // <Jh_CandidateLayout children={<CandidateDashboard/>}/>
+import { HowItWorks } from './Pages/index';
   ReactDOM.render(
     <Jh_EmployerLayout children={<HowItWorks/>}/>
   ,document.getElementById('root')
