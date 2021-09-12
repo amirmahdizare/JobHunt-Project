4fe0a6cebc1ff944cb6d1250e880a3e36ba66697
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-12 18:38:59 +0430

    Add SelectLocation & Search Keyword to JobList

--- JobHunt/jobhunt_frontend/src/Pages/JobList/JobList.js
+++ JobHunt/jobhunt_frontend/src/Pages/JobList/JobList.js
@@ -1,11 +1,24 @@
-import { Box } from '@material-ui/core'
+import { Box, Container, Grid } from '@material-ui/core'
 import React from 'react'
 import { Jh_ContentHeader } from '../../components/Jh_ContentHeader'
+import { Jh_SearchKeyword } from '../../components/Jh_SearchKeyword'
+import { Jh_SelectLocation } from '../../components/Jh_SelectLocation'
 
 export const JobList = () => {
     return (
         <Box>
             <Jh_ContentHeader image="https://creativelayers.net/themes/jobhunt-html/images/resource/mslider1.jpg" text="Job List" />
+            <Container maxWidth="lg">
+                <Grid container>
+                    <Grid item xs={12} md={3}>
+                        <Jh_SearchKeyword/>
+                        <Jh_SelectLocation/>
+                    </Grid>
+                    <Grid item xs={12} md={8}>
+                    </Grid>
+
+                </Grid>
+            </Container>
         </Box>
     )
 }
