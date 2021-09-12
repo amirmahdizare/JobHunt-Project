fd75a4841d687c2d8b8e0f5224bcbe4de50e411f
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-12 18:32:28 +0430

    Add Content Header to JobList

--- JobHunt/jobhunt_frontend/src/Pages/JobList/JobList.js
+++ JobHunt/jobhunt_frontend/src/Pages/JobList/JobList.js
@@ -1,9 +1,11 @@
+import { Box } from '@material-ui/core'
 import React from 'react'
+import { Jh_ContentHeader } from '../../components/Jh_ContentHeader'
 
 export const JobList = () => {
     return (
-        <div>
-            
-        </div>
+        <Box>
+            <Jh_ContentHeader image="https://creativelayers.net/themes/jobhunt-html/images/resource/mslider1.jpg" text="Job List" />
+        </Box>
     )
 }
