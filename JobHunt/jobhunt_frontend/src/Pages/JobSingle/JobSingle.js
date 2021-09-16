6eb4f522a55ebd2eb1447648bd64e1514558977f
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-16 12:07:29 +0430

    Add Header to JobSingle

--- JobHunt/jobhunt_frontend/src/Pages/JobSingle/JobSingle.js
+++ JobHunt/jobhunt_frontend/src/Pages/JobSingle/JobSingle.js
@@ -1,9 +1,18 @@
 import { Box } from '@material-ui/core'
 import React from 'react'
+import { Jh_JobSingleHeader } from '../../components/Jh_JobSingleHeader'
 
 export const JobSingle = () => {
     return (
         <Box>
+            <Jh_JobSingleHeader 
+            image="https://creativelayers.net/themes/jobhunt-html/images/resource/mslider1.jpg"
+            jobTitle="Senior Web Designer"
+            workTime="PART TIME"
+            location="Ajax, Ontario"
+            datePosted="Posted 4 years ago"/>
+
+
             
         </Box>
     )
