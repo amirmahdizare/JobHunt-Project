2c06320cb00c434d9319bde31d2d5f4a41d91377
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-16 14:14:09 +0430

    Add JobInfo to JobSingle

--- JobHunt/jobhunt_frontend/src/Pages/JobSingle/JobSingle.js
+++ JobHunt/jobhunt_frontend/src/Pages/JobSingle/JobSingle.js
@@ -1,19 +1,37 @@
-import { Box } from '@material-ui/core'
+import { Box, Container, Divider, Grid } from '@material-ui/core'
 import React from 'react'
 import { Jh_JobSingleHeader } from '../../components/Jh_JobSingleHeader'
+import { JobInfo } from './components/JobInfo'
 
 export const JobSingle = () => {
     return (
         <Box>
-            <Jh_JobSingleHeader 
-            image="https://creativelayers.net/themes/jobhunt-html/images/resource/mslider1.jpg"
-            jobTitle="Senior Web Designer"
-            workTime="PART TIME"
-            location="Ajax, Ontario"
-            datePosted="Posted 4 years ago"/>
+            <Jh_JobSingleHeader
+                image="https://creativelayers.net/themes/jobhunt-html/images/resource/mslider1.jpg"
+                jobTitle="Senior Web Designer"
+                workTime="PART TIME"
+                location="Ajax, Ontario"
+                datePosted="Posted 4 years ago" />
+            <Container maxWidth="lg">
+                <Grid container>
+                    <Grid item xs={12} md={8}>
+                        <JobInfo
+                        companyName="Tix Dog"
+                        companyLogo="https://creativelayers.net/themes/jobhunt-html/images/resource/sj.png"
+                        location="274 Seven Sisters Road, London, N4 2HY"
+                        website="www.jobhunt.com"
+                        phoneNumber="+90 538 963 54 87"
+                        emailAddress="ali.tufan@jobhunt.com"
+                        />
+                        <Divider light />
+                    </Grid>
+                    <Grid item xs={12} md={4}>
+                    </Grid>
+                </Grid>
+            </Container>
 
 
-            
+
         </Box>
     )
 }
