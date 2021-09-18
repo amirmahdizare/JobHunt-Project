5d9aa4c9f1585bce43f8ac209def68fb7afc40a4
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-18 17:46:17 +0430

    Create index for Layout

---
+++ JobHunt/jobhunt_frontend/src/components/Jh_layout/index.js
@@ -0,0 +1,3 @@
+export {Jh_MainLayout} from './Jh_MainLayout/Jh_MainLayout'
+export {Jh_EmployerLayout} from './Jh_EmployerLayout/Jh_EmployerLayout'
+export {Jh_CandidateLayout} from './Jh_CandidateLayout/Jh_CandidateLayout'
