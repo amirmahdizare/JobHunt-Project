9042bd37607b88dcef2bedd51643f0ffb52405ea
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-21 17:47:00 +0430

    Add HomePage to index.js

--- JobHunt/jobhunt_frontend/src/index.js
+++ JobHunt/jobhunt_frontend/src/index.js
@@ -2,14 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {Jh_EmployerLayout} from './components/Jh_layout/Jh_EmployerLayout/Jh_EmployerLayout'
-import {JobSingle } from './Pages/index';
+import {HomePage } from './Pages/index';
+import {Jh_MainLayout} from './components/Jh_layout/index'
   ReactDOM.render(
-    <Jh_EmployerLayout children={<JobSingle/>}/>
+    <Jh_MainLayout children={<HomePage/>}/>
   ,document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
