5797fe70ece9161f1b4c8feae35cc01449b25fb0
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-22 22:31:46 +0330

    Create StoreServiceId API

---
+++ JobHunt/jobhunt_frontend/src/api/initilizeApp/index.js
@@ -0,0 +1,14 @@
+import axios from "axios"
+const storeServiceIdAndDefineLanguage = async (language) =>{  
+    var config ={
+        method:'get',
+        url:`${process.env.REACT_APP_CENTRAL_API_ENDPOINT}/services`,
+        headers:{
+            'Lang':language
+        }
+    }
+    const response= await axios(config)
+    const serviceId = response.data.data.filter(item => item.name=='jobHunt')[0].id;
+    localStorage.setItem('serviceId',serviceId)
+}
+export { storeServiceIdAndDefineLanguage}
