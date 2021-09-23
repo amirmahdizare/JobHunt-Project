bc581526760f2be840653efac89cc533cd841655
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-23 10:39:59 +0330

    Update apiConfig

--- JobHunt/jobhunt_frontend/src/config-r/apiConfig.js
+++ JobHunt/jobhunt_frontend/src/config-r/apiConfig.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:REACT_APP_API_ENDPOINT
+    baseURL:process.env.REACT_APP_API_ENDPOINT
 })
 
 const centalApi = axios.create({
-    baseURL:REACT_APP_CENTRAL_API_ENDPOINT
+    baseURL:process.env.REACT_APP_CENTRAL_API_ENDPOINT
 })
 export {api,centalApi}
