4b482d5c3731938d7dc32e5ae18913443c675a09
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-24 10:20:15 +0330

    Create getPopularCategories api

---
+++ JobHunt/jobhunt_frontend/src/api/public/index.js
@@ -0,0 +1,11 @@
+import { api, centalApi } from "../../config/apiConfig"
+import { getLanguage } from "../../utils"
+const getPopularCategories = async (customParams) => {
+    const reqParams = customParams && customParams.page && customParams.pagination_size ? customParams : { page: 1, pagination_size: 6 }
+    const response = await api.get('/categories/guests', {
+        headers: { Lang: getLanguage() },
+        params: reqParams
+    })
+    return await Object.values(response.data.data)
+}
+export { getPopularCategories }
