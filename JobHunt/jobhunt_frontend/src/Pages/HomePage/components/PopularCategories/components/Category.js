0b8557e61af2d663c53d7ef84a8407e59c1ae5b5
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-24 10:45:35 +0330

    Categories APi (Unfinished)

--- JobHunt/jobhunt_frontend/src/Pages/HomePage/components/PopularCategories/components/Category.js
+++ JobHunt/jobhunt_frontend/src/Pages/HomePage/components/PopularCategories/components/Category.js
@@ -1,4 +1,4 @@
-import {Card ,Grid, makeStyles , Typography } from '@material-ui/core'
+import { Avatar, Card, Grid, Link, makeStyles, Typography } from '@material-ui/core'
 import React from 'react'
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
@@ -12,7 +12,7 @@ const useClasses = makeStyles((theme) => ({
         justifyContent: 'center',
         flexDirection: 'column',
         transition: 'all 0.5s ease',
-        borderColor:'gray',
+        borderColor: 'gray',
         '& .MuiCardActionArea-focusHighlight': {
             background: 'white'
         },
@@ -27,41 +27,55 @@ const useClasses = makeStyles((theme) => ({
             zIndex: 10,
             '& .icon': {
                 fill: theme.palette.secondary.main,
-                transition: 'all 0.4s ease'
-            }
+                transition: 'all 0.4s ease',
 
+            },
+            '& $defaultIcon': {
+                backgroundColor: theme.palette.secondary.main,
+                transition: 'all 0.4s ease',
+            }
         }
-    }, 
+    },
     content: {
         alignItems: 'center',
         display: 'flex',
         flexDirection: 'column',
         padding: theme.spacing(3),
-        
+
     },
     categoryName: {
         fontSize: '20px',
         fontWeight: '1000',
         marginTop: theme.spacing(2),
-        width:'100%',
+        width: '100%',
     },
     categoryStatus: {
         color: theme.palette.secondary.main,
         fontSize: '15px',
         marginTop: theme.spacing(2),
-        width:'100%'
+        width: '100%'
+    },
+    defaultIcon: {
+        backgroundColor: theme.palette.primary.light,
+        height: '70px',
+        width: '70px',
+        fontSize: "50px",
+
+
     }
 }))
 const Category = (props) => {
     const classes = useClasses(props);
+    const { title, openPositions, id } = props
+    const icon = !props.icon && title ? <Avatar className={classes.defaultIcon} >{title[0]}</Avatar> : props.icon
     return (
         <Grid item xs={12} sm={6} md={4} lg={3} p={2} >
-            <Card className={classes.root} >
+            <Card className={classes.root} component={Link} href={`/categories/${id}`} color="inherit" underline="none">
                 <CardActionArea>
                     <CardContent className={classes.content}>
-                        {props.icon}
-                        <Typography variant="h5" component="b" className={classes.categoryName} >{props.title}</Typography>
-                        <Typography variant="h6" className={classes.categoryStatus} >{props.status}</Typography>
+                        {icon}
+                        <Typography variant="h5" component="b" className={classes.categoryName} >{title}</Typography>
+                        {openPositions ? <Typography variant="h6" className={classes.categoryStatus} >({openPositions}&nbsp;Open Positions</Typography> : null}
                     </CardContent>
                 </CardActionArea>
             </Card>
