3ecc0faebecdd209d3a8458d43a2eb49a27841e6
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-09-30 13:26:16 +0330

    Make Loading Component

--- JobHunt/jobhunt_frontend/src/asset/index.js
+++ JobHunt/jobhunt_frontend/src/asset/index.js
@@ -1,5 +1,6 @@
 import logo from './logo/logo.png'
 import logo2 from './logo/logo2.png'
+import loader from './logo/loader.gif'
 import appstore from './downloadfromImages/appstore.png'
 import googleplay from './downloadfromImages/googleplay.png'
-export {logo,logo2,appstore,googleplay}
+export {logo,logo2,appstore,loader,googleplay}
