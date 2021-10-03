9a03473c34e60cca99335b4d9492c8a684a31ac2
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-10-03 22:14:47 +0330

    Create utils

--- JobHunt/jobhunt_frontend/src/utils/index.js
+++ JobHunt/jobhunt_frontend/src/utils/index.js
@@ -1,7 +1,34 @@
-const getServiceId =() =>{
-    return localStorage.getItem('serviceId')
+const getServiceId = () => {
+    // return localStorage.getItem('serviceId')
+    return window.sessionStorage.getItem('serviceId')
+
 }
-const getLanguage = () =>{
-    return localStorage.getItem('Lang')
+const getLanguage = () => {
+    return window.sessionStorage.getItem('Lang')
+    // return localStorage.getItem('Lang')
 }
-export { getServiceId,getLanguage}
+const storeToken = (data) => {
+    window.sessionStorage.setItem('token', data.data.token)
+    // localStorage.setItem('token', data.data.token)
+}
+const getToken =() =>{
+    // return 'Bearer '+localStorage.getItem('token')
+    return 'Bearer '+window.sessionStorage.getItem('token')
+}
+// const deleteTokenfromLocalStorage = () =>{
+//     localStorage.removeItem('')
+// }
+const storeUsertoken = () =>{
+    localStorage.setItem('userToken', getToken())
+}
+
+const getUserToken= () =>{
+    localStorage.getItem('userToken')
+}
+const centralApiHeaderObj = () => {
+    return {
+        'Service-ID':getServiceId(),
+        'Lang':getLanguage()
+    }
+}
+export { getServiceId, getLanguage,storeToken,getToken ,centralApiHeaderObj ,storeUsertoken,getUserToken}
