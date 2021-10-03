46d0fb6d3df822b10821a828daf43fb59af41d79
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-10-03 22:11:21 +0330

    Add LogonPage to route

--- JobHunt/jobhunt_frontend/src/routes/PublicRoute.js
+++ JobHunt/jobhunt_frontend/src/routes/PublicRoute.js
@@ -1,10 +1,11 @@
-import React, { Fragment } from 'react'
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import { Jh_MainLayout } from '../components/Jh_layout'
 import { CommonRoutes } from './components/CommonRoutes'
 import { RouteMaker } from './components/RouteMaker'
 const ExclusivePublicRoutes = [
     { path: "/forgotpassword", pageName: 'ForgotPassword' },
+    { path: "/login", pageName: 'LoginPage' },
     { pageName: 'PublicNotFoundPage' }
 ]
 const PublicRoute = () => {
