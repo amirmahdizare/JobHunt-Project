4e9a28d39db7202fadacfd95c8bba6024e42e949
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-10-08 18:09:03 +0330

    Create API for getting ValidCountries to

--- JobHunt/jobhunt_frontend/src/api/public/index.js
+++ JobHunt/jobhunt_frontend/src/api/public/index.js
@@ -1,5 +1,7 @@
 import { api, centralApi } from "../../config/apiConfig"
-import { getLanguage } from "../../utils"
+import { centralApiHeaderObj, getLanguage } from "../../utils"
+import { countriesToSignUpConfig } from "../../config/countriesToSignUpConfig"
+import { generateImageURL } from "../OSS/minioAPI"
 const getPopularCategories = async (customParams) => {
     const reqParams = customParams && customParams.page && customParams.pagination_size ? customParams : { page: 1, pagination_size: 6 }
     const response = await api.get('/categories/guests', {
@@ -8,4 +10,40 @@ const getPopularCategories = async (customParams) => {
     })
     return await Object.values(response.data.data)
 }
-export { getPopularCategories }
+
+const getSingleCountryInfoToSignup = async (countryName) => {
+    const response = await centralApi.get('/countries', {
+        params: {
+            page: 1,
+            pagination_size: 3,
+            name: countryName
+        },
+        headers: centralApiHeaderObj()
+    })
+
+    return response.data.data.entities[0]
+}
+
+const getValidCountriesToSignupDetail = async () => {
+    const pureCountryDetail = await Promise.all(countriesToSignUpConfig.map(async(country) => {
+        try {
+            const countryDetail = await getSingleCountryInfoToSignup(country)
+            return countryDetail
+
+        } catch (error) {
+            // break
+        }
+    }))
+    const FullCountryDetail = await Promise.all(pureCountryDetail.map(async(country) => {
+        try {
+            const flagIcon = await generateImageURL(country['flag'][country.id]['path'])
+            return { ...country, 'flagIcon': flagIcon }
+        } catch (error) {
+            //break
+        }
+    }
+    ))
+    console.log(FullCountryDetail)
+    return FullCountryDetail
+}
+export { getPopularCategories, getValidCountriesToSignupDetail }
