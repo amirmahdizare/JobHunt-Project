37546fef870692257cd957c5cad4033e009b6b19
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-12-03 11:20:46 +0330

    Modify Footer Content And Style

--- JobHunt/jobhunt_frontend/src/components/Jh_layout/components/Footer/components/FindJobs.js
+++ JobHunt/jobhunt_frontend/src/components/Jh_layout/components/Footer/components/FindJobs.js
@@ -4,22 +4,22 @@ import { ListMenu } from './ListMenu'
 import { Typography } from '@material-ui/core'
 const FindJobs = () => {
     return (
-        <Grid item  xs={12} md={3} >
-        <Box p={2}>
-            <Typography variant="h5" style={{ color: 'white' }}>
-                Find Jobs
-            </Typography>
-            <ListMenu
-                menuItems={[
-                    { name: 'US Jobs', href: '#' },
-                    { name: 'Canada Jobs', href: '#' },
-                    { name: 'UK Jobs', href: '#' },
-                    { name: 'Emplois en Fnce', href: '#' },
-                    { name: 'Join in Deuts', href: '#' },
-                    { name: 'Vacatures China', href: '#' }
-                ]}
-            />
-        </Box>
+        <Grid item xs={12} md={3} >
+            <Box p={2}>
+                <Typography variant="h5" style={{ color: 'white' }}>
+                    Find Jobs
+                </Typography>
+                <ListMenu
+                    menuItems={[
+                        { name: 'Beijing Jobs', href: '#' },
+                        { name: 'Shanghai Jobs', href: '#' },
+                        { name: 'Shenzhen Jobs', href: '#' },
+                        { name: 'Guangzhou Jobs', href: '#' },
+                        { name: 'Online or Freelancer', href: '#' },
+                        { name: 'jobhunt Academy', href: '#' }
+                    ]}
+                />
+            </Box>
         </Grid>
     )
 }
