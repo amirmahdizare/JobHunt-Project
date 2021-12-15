79a3bd03c54bdacd410772081203e4e95ae270ee
unknown <amirhosseinrazavi_76@yahoo.com>
2021-12-15 15:13:44 +0330

    procces .env

--- JobHunt/jobhunt_frontend/src/config/apiConfig.js
+++ JobHunt/jobhunt_frontend/src/config/apiConfig.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL:process.env.REACT_APP_API_ENDPOINT
-    baseURL: 'https://jobhuntapi.stg.blytd.com'
+    baseURL:process.env.REACT_APP_API_ENDPOINT
 })
 
 const centralApi = axios.create({
