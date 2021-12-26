263ad9307348ef3e18d51a8487e37f7dcf95a904
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-12-26 18:59:32 +0330

    Implement Transaction Section

--- JobHunt/jobhunt_frontend/src/Pages/EmployerDashboard/EmployerDashboard.js
+++ JobHunt/jobhunt_frontend/src/Pages/EmployerDashboard/EmployerDashboard.js
@@ -8,6 +8,7 @@ import { ChangePassword } from './components/ChangePassword/ChangePassword'
 import { CompanyProfile } from './components/CompanyProfile/CompanyProfile'
 import { ManageJobs } from './components/ManageJobs/ManageJobs'
 import { PostJob } from './components/PostJob/PostJob'
+import { Transactions } from './components/Transactions/Transactions'
 
 const EmployerDashboard = () => {
     let { path, url } = useRouteMatch();
@@ -16,15 +17,15 @@ const EmployerDashboard = () => {
             <ContentHeader image={CandidatesDashboardBackground} text="Welcome Tara Planer" />
             <Container maxWidth="lg">
                 <Grid container >
-                    <Grid item xs={12} md={3}>
+                    <Grid item xs={12} md={2}>
                         <EmployerMenu />
                     </Grid>
                     <Divider light orientation="vertical" flexItem />
-                    <Grid item xs={12} md={8}>
+                    <Grid item xs={12} md>
                         <Switch>
                             <Route path={`${path}/managejobs`} component={ManageJobs} />
                             <Route exact path={`${path}/companyprofile`} component={CompanyProfile} />
-                            <Route exact path={`${path}/transactions`} children={<h1>Transactions</h1>} />
+                            <Route exact path={`${path}/transactions`} children={Transactions} />
                             <Route exact path={`${path}/resumes`} children={<h1>Resumes</h1>} />
                             <Route exact path={`${path}/packages`} children={<h1>Packages</h1>} />
                             <Route exact path={`${path}/postjob`} component={PostJob} />
