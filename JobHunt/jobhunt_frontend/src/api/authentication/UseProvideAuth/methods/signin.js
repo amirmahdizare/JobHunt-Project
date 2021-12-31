d57effdf929552de775dde357f164e3ead0de3bd
amirmahdi <amirmahdikeshanzare@gmail.com>
2021-12-31 19:01:12 +0330

    Fix Login Issue

--- JobHunt/jobhunt_frontend/src/api/authentication/UseProvideAuth/methods/signin.js
+++ JobHunt/jobhunt_frontend/src/api/authentication/UseProvideAuth/methods/signin.js
@@ -2,13 +2,14 @@ import { centralApi } from "../../../../config/apiConfig"
 import { centralApiHeaderObj, storeToken, storeUser } from "../../../../utils";
 import { _getRoll } from "./getRoll";
 
-const _signin = async function (setUser, { identifier, password }) {
+const _signin = async function (setUser, {country_code, identifier, password }) {
   var data = new FormData();
   data.append('identifier', identifier);
   data.append('password', password);
+  data.append('country_code', country_code);
   try {
     
-    const response = await centralApi.post('/login', data, {
+    const response = await centralApi.post('/login?ident_kind=mobile', data, {
       headers: centralApiHeaderObj()
     })
     
