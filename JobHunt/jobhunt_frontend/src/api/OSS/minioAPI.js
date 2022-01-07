b2d363b23634068554f49e703d7d087422953b97
amirmahdi <amirmahdikeshanzare@gmail.com>
2022-01-07 11:28:55 +0330

    Exclude Minio and Oss from Project

--- JobHunt/jobhunt_frontend/src/api/OSS/minioAPI.js
+++ JobHunt/jobhunt_frontend/src/api/OSS/minioAPI.js
@@ -1,17 +1,7 @@
-import * as minio from "minio";
 const generateImageURL = async (source, id) => {
-    const minioClient = new minio.Client({
-        endPoint: process.env.REACT_APP_API_OSS_ENDPOINT,
-        accessKey: process.env.REACT_APP_API_OSS_ACCESSKEY,
-        secretKey: process.env.REACT_APP_API_OSS_SECRETKEY
-        // port: 9000,
-        // useSSL: true,
-    });
-    if (process.env.REACT_APP_API_ENDPOINT == "https://jobhuntapi.stg.blytd.com")
-        var photoUrl = await minioClient.presignedUrl('GET', `stage-${source}`, id, 24 * 60 * 60);
-    else {
-        var photoUrl = await minioClient.presignedUrl('GET', process.env.REACT_APP_API_OSS_POCKET, id, 24 * 60 * 60);
-    }
-    return photoUrl
+    return source=='jobhunt' 
+    ? process.env.REACT_APP_API_ENDPOINT + '/file?path=' + id
+    : process.env.REACT_APP_CENTRAL_API_ENDPOINT +  '/file?path=' + id
+
 }
 export { generateImageURL }
